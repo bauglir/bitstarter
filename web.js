fb3e4f3de404aac244f2ce51d1f3fcd1c0ab61cc
Joris Kraak <me@joriskraak.nl>
2013-07-11 12:11:38 +0200

    Send index.html instead of static string from web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  indexFileBuffer = fs.readFileSync('index.html');
+  response.send(indexFileBuffer.toString());
 });
 
 var port = process.env.PORT || 5000;
